02fa511ccb9d7d5e20bcd8de21e31b5281eef6bb
Amitabh Yadav <amitabhydv@gmail.com>
2018-03-20 10:25:10 +0100

    Keyboard Handler Update
    Updated the KeyPressed Function.
    keyReleased does nothing; keyTyped not required and also does nothing

--- snake_game/src/main/java/athensproject/KeyboardHandler.java
+++ snake_game/src/main/java/athensproject/KeyboardHandler.java
@@ -10,11 +10,22 @@ public class KeyboardHandler implements KeyListener {
     }
 
     public void keyPressed(KeyEvent keyEvent) {
-
-        ;//This should change the snake movement direction invoking the changeDirection method of snake
+    	int key = keyEvent.getKeyCode();
+    	if(key == keyEvent.VK_UP){
+    		Snake.changeDirection(MoveDirections.UP);
+    	}
+    	if(key == keyEvent.VK_DOWN){
+    		Snake.changeDirection(MoveDirections.DOWN);
+    	}
+    	if(key == keyEvent.VK_LEFT){
+    		Snake.changeDirection(MoveDirections.LEFT);
+    	}
+    	if(key == keyEvent.VK_RIGHT){
+    		Snake.changeDirection(MoveDirections.RIGHT);
+    	}//This should change the snake movement direction invoking the changeDirection method of snake
     }
 
     public void keyReleased(KeyEvent keyEvent) {
-        ;
+        //The change direction is implemented in the KetPressed(). The KeyReleased needs not be implemented.
     }
 }
